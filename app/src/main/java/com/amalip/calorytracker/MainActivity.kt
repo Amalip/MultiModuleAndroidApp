7d5a85e02c936f9f06a5f794e4d4b2c5fc5cb403
Amauri Guzmán <amauri.guzman@dacodes.com.mx>
2022-02-15 11:56:38 -0600

    Init project

---
+++ app/src/main/java/com/amalip/calorytracker/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.amalip.calorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.amalip.calorytracker.ui.theme.CaloryTrackerTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+
+            }
+        }
+    }
+
+}
